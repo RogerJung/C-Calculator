984a6f9029bedf3635e7bae7df9c25b74d6e3094
RogerJung <engineer54rj@gmail.com>
2022-08-08 14:51:30 +0800

    NoteBook  test
    test pass.

--- main/MVVMExample/View/QueryView.xaml.cs
+++ main/MVVMExample/View/QueryView.xaml.cs
@@ -1,23 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using DataGrid = System.Windows.Controls.DataGrid;
-using DataGridCell = System.Windows.Controls.DataGridCell;
 
 namespace MVVMExample.View
 {
