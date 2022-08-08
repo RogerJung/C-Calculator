df094eb2344a76a96eba8446a6588a536cb260b2
RogerJung <engineer54rj@gmail.com>
2022-08-08 12:51:59 +0800

    Project finished
    congratulation!

--- main/MVVMExample/View/QueryView.xaml.cs
+++ main/MVVMExample/View/QueryView.xaml.cs
@@ -26,61 +26,53 @@ namespace MVVMExample.View
     /// </summary>
     public partial class QueryView : Window
     {
+        //setting mysql connect
+        string connectionString = "Data Source=127.0.0.1;User ID=root;Password=;DataBase=calculate;Charset=utf8;";
+        MySqlConnection con;
+        MySqlDataAdapter adapter;
+        DataSet ds;
+        DataTable dt;
+
         public QueryView()
         {
             InitializeComponent();
+
+            UpdateMySQLData();
         }
 
-        DataTable dt = new DataTable();
-        ObservableCollection<char[]> showdata = new ObservableCollection<char[]>();
-
-        private void dataTable_Loaded(object sender, RoutedEventArgs e)
+        private void UpdateMySQLData()
         {
-            //setting mysql connect
-            String connectionString = "dataSource=localhost;username=root;PASSWORD=;";
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            connection.Open();
-            MySqlCommand cmd = new MySqlCommand("select * from calculate.data", connection);
-            MySqlDataReader data = cmd.ExecuteReader();
-            dataGrid.ItemsSource = data;
+            if (con == null)
+            {
+                con = new MySqlConnection(connectionString);
+                con.Open();
+            }
+            if (adapter == null)
+            {
+                adapter = new MySqlDataAdapter("select * from data", con);
+            }
+            if (ds == null)
+            {
+                ds = new DataSet();
+            }
+            ds.Clear();
+            adapter.Fill(ds, "data");
+            if (dt == null)
+            {
+                dt = ds.Tables["data"];
+            }
+            dataGrid.ItemsSource = dt.DefaultView;
         }
-
-        public string selectedData = null;
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            String connectionString = "dataSource=localhost;username=root;PASSWORD=;";
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            connection.Open();
-            MySqlCommand cmd = new MySqlCommand("Delete from calculate.data where Inorder=\'" + selectedData + "\'", connection);
-            MySqlDataReader data = cmd.ExecuteReader();
-            dataGrid.ItemsSource = data;
-            dataTable_Loaded(sender, e);
-        }
+            int index = dataGrid.SelectedIndex;
+            if (index == -1) return;
 
-        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var cells = dataGrid.SelectedCells;
-            StringBuilder sb = new StringBuilder();
-            if (cells.Any())
-            {
-                foreach(var cell in cells)
-                {
-                    sb.Append((cell.Column.GetCellContent(cell.Item) as TextBlock).Text);
-                    sb.Append(" ");
-                }
-                string[] words = sb.ToString().Split(' ');
-                System.Console.WriteLine(words[1]);
-                selectedData = words[1];
-            }
-            /*DataGrid dataGrid1 = sender
-             * 
-             * as DataGrid;
-            DataGridRow row = (DataGridRow)dataGrid1.ItemContainerGenerator.ContainerFromIndex(dataGrid1.SelectedIndex);
-            DataGridCell RowColumn = dataGrid1.Columns[1].GetCellContent(row).Parent as DataGridCell;
-            string CellValue = ((TextBlock)RowColumn.Content).Text;
-            System.Console.WriteLine(CellValue);
-            selectedData = CellValue;*/
+            dt.Rows[index].Delete();
+            MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+            adapter.Update(dt);
+            dt.AcceptChanges();
         }
     }
 }
